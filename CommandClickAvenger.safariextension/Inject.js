6c9316327eb85eef59d74e249c37cc03cdf0b174
Peter Maurer <peter@manytricks.com>
2013-04-30 00:04:41 +0200

    Improved the way we deal with classic onclick handlers: We now replace them and call the original ones unless cmd is held. Added an event listener to deal with sites like ESPN's. Updated description.

--- CommandClickAvenger.safariextension/Inject.js
+++ CommandClickAvenger.safariextension/Inject.js
@@ -1,27 +1,53 @@
 // Copyright © 2013 Many Tricks (When in doubt, consider this MIT-licensed)
 
-var theElementList = document.getElementsByTagName('*');
+function referenceForElement(theElement) {
+	if (theElement.nodeName=='A') {
+		return theElement.href;			// identify proper links or...
+	}
+	var theNestedAnchors = theElement.getElementsByTagName('A');
+	var anIndex;
+	var aReference;
+	for (anIndex = 0; anIndex<theNestedAnchors.length; anIndex++) {
+		aReference = theNestedAnchors[anIndex].href;
+		if (aReference) {
+			return aReference;			// ...elements that contain at least one proper link
+		}
+	}
+	return null;
+}
+
+
+// Step 1: Deal with onclick handlers by doing one of two things (cf. replaceOnclickHandlers):
+//
+// - Replace them with an onclick handler that calls the original handler unless cmd is held
+// - Simply remove offending onclick handlers
+
+var replaceOnclickHandlers = true;
+
+var theElements = document.getElementsByTagName('*');
 var anElementIndex;
 var anElement;
 var anOnclickHandler;
-var anAnchorList;
-var anAnchorIndex;
-for (anElementIndex = 0; anElementIndex<theElementList.length; anElementIndex++) {
-	anElement = theElementList[anElementIndex];
+for (anElementIndex = 0; anElementIndex<theElements.length; anElementIndex++) {
+	anElement = theElements[anElementIndex];
 	anOnclickHandler = anElement.onclick;
-	if ((anOnclickHandler) && (('Lazy String Conversion: ' + anOnclickHandler).indexOf('.location.href=')!==-1)) {
-		if (anElement.nodeName=='A') {
-			if (anElement.href) {
-				anElement.removeAttribute('onclick');			// knock out onclick handlers for proper links or...
+	if ((anOnclickHandler) && (referenceForElement(anElement))) {
+		if (replaceOnclickHandlers) {
+			anElement.com_manytricks_commandclickavenger_originalonclick = anOnclickHandler;
+			anElement.onclick = function(theEvent) {
+				return (((theEvent) ? theEvent : window.event).metaKey || (this.com_manytricks_commandclickavenger_originalonclick.call(this, theEvent)!==false));
 			}
-		} else {
-			anAnchorList = anElement.getElementsByTagName('A');
-			for (anAnchorIndex = 0; anAnchorIndex<anAnchorList.length; anAnchorIndex++) {
-				if (anAnchorList[anAnchorIndex].href) {
-					anElement.removeAttribute('onclick');		// ...elements that contain at least one proper link
-					break;
-				}
-			}
+		} else if (('Lazy String Conversion: ' + anOnclickHandler).indexOf('.location.href=')!==-1) {
+			anElement.removeAttribute('onclick');
 		}
 	}
 }
+
+
+// Step 2: Knock out modern event listeners with our own event listener if the cmd key is held
+
+window.addEventListener('click', function(theEvent) {
+	if (theEvent.metaKey && (referenceForElement(theEvent.target))) {
+		theEvent.stopPropagation();
+	}
+}, true);
